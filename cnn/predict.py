cec715476cc747d542bd1aff53f407845f1e5165
threelittlemonkeys <sjlee7642@gmail.com>
2018-11-08 16:46:26 +0900

    Update by commit

--- cnn/predict.py
+++ cnn/predict.py
@@ -17,22 +17,23 @@ def run_model(model, idx_to_tag, data):
     batch = []
     z = len(data)
     while len(data) < BATCH_SIZE:
-        data.append(["", [], ""])
-    data.sort(key = lambda x: len(x[1]), reverse = True)
-    batch_len = len(data[0][1])
-    batch = [[SOS_IDX] + x[1] + [EOS_IDX] + [PAD_IDX] * (batch_len - len(x[1])) for x in data]
+        data.append([-1, "", [], ""])
+    data.sort(key = lambda x: len(x[2]), reverse = True)
+    batch_len = len(data[0][2])
+    batch = [[SOS_IDX] + x[2] + [EOS_IDX] + [PAD_IDX] * (batch_len - len(x[2])) for x in data]
     result = model(LongTensor(batch))
     for i in range(z):
-        data[i] = (data[i][0], data[i][2], idx_to_tag[result[i].argmax()])
+        data[i] = (data[i][0], data[i][1], data[i][3], idx_to_tag[result[i].argmax()])
         if VERBOSE:
             print()
             print(data[i])
             y = torch.exp(result[i]).tolist()
             for j, p in sorted(enumerate(y), key = lambda x: x[1], reverse = True):
                 print("%s %.6f" % (idx_to_tag[j], p))
-    return data[:z]
+    return [x[1:] for x in sorted(data[:z])]
 
 def predict(lb = False):
+    idx = 0
     data = []
     result = []
     model, word_to_idx, tag_to_idx, idx_to_tag = load_model()
@@ -42,10 +43,12 @@ def predict(lb = False):
         line, y = line.split("\t") if lb else [line, ""]
         x = tokenize(line, UNIT)
         x = [word_to_idx[i] if i in word_to_idx else UNK_IDX for i in x]
-        data.append([line, x, y])
+        data.append([idx, line, x, y])
         if len(data) == BATCH_SIZE:
             result.extend(run_model(model, idx_to_tag, data))
+            idx = 0
             data = []
+        idx += 1
     fo.close()
     if len(data):
         result.extend(run_model(model, idx_to_tag, data))
