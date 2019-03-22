532eb5b0f09331c78a849fae8b5b782802b5a423
threelittlemonkeys <sjlee7642@gmail.com>
2019-03-22 09:58:41 +0900

    Update by commit

--- rnn/predict.py
+++ rnn/predict.py
@@ -59,5 +59,5 @@ if __name__ == "__main__":
     with torch.no_grad():
         result = predict(sys.argv[4], False, *load_model())
         print()
-        for x, _, y, p in result:
-            print((x, y, p))
+        for x, y0, y1, p in result:
+            print((x, y1, p))
