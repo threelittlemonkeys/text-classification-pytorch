d5a0c89e3eb4ad1804f85c25ed3e93e42ca9e1dc
threelittlemonkeys <sjlee7642@gmail.com>
2019-04-06 01:38:05 +0900

    Update by commit

--- utils.py
+++ utils.py
@@ -7,6 +7,9 @@ from parameters import *
 from collections import defaultdict
 
 def normalize(x):
+    # x = re.sub("[\uAC00-\uD7A3]+", "\uAC00", x) £ convert Hangeul to 가
+    # x = re.sub("[\u3040-\u30FF]+", "\u3042", x) # convert Hiragana and Katakana to あ
+    # x = re.sub("[\u4E00-\u9FFF]+", "\u6F22", x) # convert CJK unified ideographs to 漢
     x = re.sub("\s+", " ", x)
     x = re.sub("^ | $", "", x)
     x = x.lower()
@@ -52,9 +55,10 @@ def save_tkn_to_idx(filename, tkn_to_idx):
     fo.close()
 
 def load_checkpoint(filename, model = None):
-    print("loading model...")
+    print("loading %s" % filename)
     checkpoint = torch.load(filename)
-    model.load_state_dict(checkpoint["state_dict"])
+    if model:
+        model.load_state_dict(checkpoint["state_dict"])
     epoch = checkpoint["epoch"]
     loss = checkpoint["loss"]
     print("saved model: epoch = %d, loss = %f" % (checkpoint["epoch"], checkpoint["loss"]))
@@ -63,7 +67,7 @@ def load_checkpoint(filename, model = None):
 def save_checkpoint(filename, model, epoch, loss, time):
     print("epoch = %d, loss = %f, time = %f" % (epoch, loss, time))
     if filename and model:
-        print("saving model...")
+        print("saving %s" % filename)
         checkpoint = {}
         checkpoint["state_dict"] = model.state_dict()
         checkpoint["epoch"] = epoch
@@ -119,6 +123,4 @@ def mat2csv(m, ch = True, rh = False, nd = NUM_DIGITS, delim ="\t"):
     return csv
 
 def f1(p, r):
-    if p + r:
-        return 2 * p * r / (p + r)
-    return 0
+    return 2 * p * r / (p + r) if p + r else 0
